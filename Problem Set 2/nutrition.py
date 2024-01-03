04f9bfebfc0e6f8f89322e9bc076b2ea81ab6749
NajmusSakib987 <nazmussakib9589@gmail.com>
2024-01-03 16:30:09 +0600

    Create nutrition.py

---
+++ Problem Set 2/nutrition.py
@@ -0,0 +1,32 @@
+fruits ={
+    'Apple' : 130,
+    'Avocado': 50,
+    'Banana' : 110,
+    'Cantaloupe' : 50,
+    'Grapefruit' : 60,
+    'Grapes' : 90,
+    'Honeydew Melon' : 50,
+    'Kiwifruit' : 90,
+    'Lemon' : 15,
+    'lime' : 20,
+    'Nectarine' : 60,
+    'Orange' : 80,
+    'Peach' : 60,
+    'Pear' : 100,
+    'Pine Apple' : 50,
+    'Plums' : 70,
+    'Strawberries' : 50,
+    'Sweet Cherries' : 100,
+    'Tangerine' : 50,
+    'Watermelon' : 80
+ }
+
+
+user = input('Fruit: ')
+user = user.title().strip()
+
+if user in fruits:
+    print(f'Calories: {fruits[user]}')
+else:
+    pass
+
